a016e49350b7ecf26aa1b7b8ffe059c69679e1fc
David Unelind <david.unelind@gmail.com>
2023-06-27 11:42:08 +0200

    Delete Python Code Challenges directory

--- Python Code Challenges/3. Sort a string.py
+++
@@ -1,6 +0,0 @@
-def sortString(text):
-    words = text.split(" ")
-    words.sort(key = lambda w: w.upper())
-    return words
-
-print(sortString("banana ORANGE apple")) #['apple', 'banana', 'ORANGE']
